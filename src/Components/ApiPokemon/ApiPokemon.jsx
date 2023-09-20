ef55b7a9e3a0e0c27746816937d30f472d81f764
lucas <lucasgeansantos@gmail.comgit config --global user.name lucasgit config --global user.email lucasgeansantos@gmail.comgit config --global user.email lucasgeansantos@gmail.com>
2023-09-20 09:32:37 -0300

    add photo of project

--- src/Components/ApiPokemon/ApiPokemon.jsx
+++ src/Components/ApiPokemon/ApiPokemon.jsx
@@ -9,7 +9,7 @@ function ApiPokemon() {
 
     async function loadApi(e) {
         e?.preventDefault()
-        let url = `https://pokeapi.co/api/v2/pokemon/${selectPokemon}`
+        let url = `https://pokeapi.co/api/v2/pokemon/${selectPokemon ? selectPokemon : 'pikachu'}`
         await fetch(url)
             .then(response => response.json())
             .then(json => {
@@ -34,22 +34,24 @@ function ApiPokemon() {
     return (
         <div className='container'>
             <header>
-                <strong>Pokemon Api</strong>
+                <strong>PokéDex</strong>
             </header>
 
-            <div style={{ marginTop: 50, color: 'black' }}>
+            <div style={{ marginTop: 50, color: 'black', display: 'flex', flexDirection:'column', alignItems:'center' }}>
                 <div style={{ display: 'flex', justifyContent: "center", alignItems: "center" }}>
                     <form onSubmit={loadApi}>
                         <input required type="text" placeholder='Nome do pokemon' value={selectPokemon} alt="" onChange={(text) => setSelectPokemon(text.target.value)} />
-                        <button type='submit'>Buscar</button>
+                        <button type='submit' style={{ marginLeft: 20, cursor: 'pointer' }}>Buscar</button>
                     </form>
                 </div>
                 {error && <p style={{ color: 'red', fontSize: 12 }}>Este pokemon nao exite, verifique a ortografia e lembre-se de colocar tudo minúsculo</p>}
-                <img src={pokemon?.sprites?.front_default} alt={pokemon?.name} />
-                <div>Name: {pokemon?.name}</div>
-                <div>Nº: {pokemon?.id}</div>
-                <div>Peso: {pokemon?.weight / 10}</div>
-                <div>Altura: {pokemon?.height / 10}</div>
+                <img src={pokemon?.sprites?.front_default} alt={pokemon?.name} style={{ width: 150 }} />
+                <div style={{color:'white', fontSize:20, textAlign:'center', fontWeight:600, fontFamily:'cursive'}}>
+                    <div>Name: {pokemon?.name}</div>
+                    <div>Nº: {pokemon?.id}</div>
+                    <div>Peso: {pokemon?.weight / 10}</div>
+                    <div>Altura: {pokemon?.height / 10}</div>
+                </div>
             </div>
         </div>
     )
